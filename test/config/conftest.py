7cc6a7276d706297ee986331da6aa901bc884f6c
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-10-04 18:42:34 +1000

    Refactor some fixtures

--- test/config/conftest.py
+++ test/config/conftest.py
@@ -5,23 +5,19 @@ import pytest
 
 @pytest.fixture
 def bad_config_path():
-    return (Path(__file__).parent / "files/bad_json_data.json").resolve()
+    return _path("files/bad_json_data.json")
 
 @pytest.fixture
 def non_existent_config_path():
-    return (Path(__file__).parent / "files/non_existent.json").resolve()
+    return _path("files/non_existent.json")
 
 @pytest.fixture
 def non_array_env_var_names_config_path():
-    return (
-        (Path(__file__).parent / "files/non_array_env_var_names.json").resolve()
-    )
+    return _path("files/non_array_env_var_names.json")
 
 @pytest.fixture
 def valid_env_var_names_mac_linux_config_path():
-    path = (
-        Path(__file__).parent / "files/valid_env_var_names_mac_linux.json"
-    ).resolve()
+    path = _path("files/valid_env_var_names_mac_linux.json")
     with path.open(encoding="utf-8") as file:
         config_data = json.load(file)
         file.close()
@@ -34,9 +30,7 @@ def valid_env_var_names_mac_linux_config_path():
 
 @pytest.fixture
 def valid_env_var_names_windows_config_path():
-    path = (
-        Path(__file__).parent / "files/valid_env_var_names_windows.json"
-    ).resolve()
+    path = _path("files/valid_env_var_names_windows.json")
     with path.open(encoding="utf-8") as file:
         config_data = json.load(file)
         file.close()
@@ -46,3 +40,6 @@ def valid_env_var_names_windows_config_path():
     with path.open("w", encoding="utf-8") as file:
         json.dump(config_data, file, indent=2)
         file.close()
+
+def _path(path):
+    return (Path(__file__).parent / path).resolve()
